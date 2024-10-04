2b101fb0bae0596588d18e7beaded6dc6ff83384
TheSoftNode <thesoftnode@gmail.com>
2024-10-04 11:37:19 +0100

    Added the theme provider and toggler

---
+++ components/Theme/ThemeProvider.tsx
@@ -0,0 +1,18 @@
+"use client";
+
+import React, { FC } from "react";
+import { ThemeProvider } from "next-themes";
+
+type Props = {
+  children: React.ReactNode;
+};
+
+const ThemeProviders: FC<Props> = ({ children }) => {
+  return (
+    <ThemeProvider attribute="class" defaultTheme="light">
+      {children}
+    </ThemeProvider>
+  );
+};
+
+export default ThemeProviders;
