6f009b8db1743db8e3136b906cf354d61ae66cbf
TheSoftNode <thesoftnode@gmail.com>
2024-10-04 10:50:24 +0100

    Changed to nextjs for better optimisation and functionalities

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,12 +1,19 @@
 import type { Config } from "tailwindcss";
 
 const config: Config = {
+  darkMode: ["class"],
   content: [
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
+    screens: {
+      sm: "640px",
+      md: "768px",
+      lg: "1024px",
+      xl: "1400px",
+    },
     extend: {
       colors: {
         background: "var(--background)",
