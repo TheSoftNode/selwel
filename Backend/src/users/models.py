4747c765cf909fa10e9384e801427c0171d4926e
TheSoftNode <thesoftnode@gmail.com>
2024-10-06 23:01:45 +0100

    Added the user model

--- Backend/src/users/models.py
+++ Backend/src/users/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
+
 # Create your models here.
+class User(models.Model):
+    country = models.TextField(blank=True, null=True)
+    accountType = models.TextField(blank=True, null=True)
+    email = models.EmailField()
+    password = models.TextField(blank=True, null=True)
+    updated = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField(auto_now_add=True)
