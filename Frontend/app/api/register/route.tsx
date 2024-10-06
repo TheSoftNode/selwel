1995c44fab345ddb7bb6d14419ca370f76696f10
TheSoftNode <thesoftnode@gmail.com>
2024-10-07 00:35:54 +0100

    Add the register route endpoints on frontend

---
+++ Frontend/app/api/register/route.tsx
@@ -0,0 +1,17 @@
+import { NextResponse } from "next/server";
+import ApiProxy from "../proxy";
+import { DJANGO_API_ENDPOINT } from "@/config/defaults";
+
+const DJANGO_API_USERS_URL=`${DJANGO_API_ENDPOINT}/users/`
+
+export async function GET(request: Request){
+    const {data, status} = await ApiProxy.get(DJANGO_API_USERS_URL, true)
+    return NextResponse.json(data, {status: status})
+}
+
+
+export async function POST(request: Request) {
+    const requestData = await request.json()
+    const {data, status} = await ApiProxy.post(DJANGO_API_USERS_URL, requestData, true )
+    return NextResponse.json(data, {status: status})
+}   
