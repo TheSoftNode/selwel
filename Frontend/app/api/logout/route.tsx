87ef6726392e7817ddd567305f7a2178bbf6d471
TheSoftNode <thesoftnode@gmail.com>
2024-10-07 00:24:15 +0100

    Add the logout route on the frontend

---
+++ Frontend/app/api/logout/route.tsx
@@ -0,0 +1,9 @@
+import { deleteTokens } from "@/lib/auth";
+import { NextResponse } from "next/server";
+
+
+export async function POST(request: Request)
+{
+    deleteTokens()
+    return NextResponse.json({}, { status: 200 })
+}
