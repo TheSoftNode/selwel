2ef760561cc9be811ab059fa6fdac97611d75b14
TheSoftNode <thesoftnode@gmail.com>
2024-10-06 23:44:43 +0100

    mounted user route on the main api

--- Backend/src/selwellhome/api.py
+++ Backend/src/selwellhome/api.py
@@ -1,4 +1,4 @@
-from ninja import NinjaAPI, Schema
+from ninja import Schema
 
 from ninja_extra import NinjaExtraAPI
 from ninja_jwt.authentication import JWTAuth
@@ -7,6 +7,7 @@ from ninja_jwt.controller import NinjaJWTDefaultController
 
 api = NinjaExtraAPI()
 api.register_controllers(NinjaJWTDefaultController)
+api.add_router("/users/", "users.api.router")
 
 class UserSchema(Schema):
     country: str = None
